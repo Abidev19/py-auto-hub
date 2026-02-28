82617da058d016af145e45028a68d86461f966ef
Abidev19 <abikhy1116@gmail.com>
2026-02-28 13:11:33 +0300

    Update iftar.py

--- iftar.py
+++ iftar.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach",True)
-driver = webdriver.Chrome(options=chrome_options)
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+# chrome_options.add_experimental_option("detach",True)
+driver = webdriver.Chrome(options=options)
 
 driver.get("https://uygulama.kulder.org/Giris")
 
@@ -29,3 +34,4 @@ basvur_success.click()
 
 # time.sleep(6)
 driver.close()
+
